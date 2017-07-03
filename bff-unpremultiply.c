3d575088436f16f9fcbc86c0674a396cb218df52
Mattias Andrée <maandree@kth.se>
2017-07-03 23:13:56 +0200

    whoops...
    Signed-off-by: Mattias Andrée <maandree@kth.se>

---
+++ bff-unpremultiply.c
@@ -0,0 +1,23 @@
+/* See LICENSE file for copyright and license details. */
+#include "common.h"
+
+USAGE("")
+
+int
+main(int argc, char *argv[])
+{
+	pid_t pid;
+
+	UNOFLAGS(argc);
+
+	bff_begin();
+
+	pid = bff_run((const char *[]){ "blind-unpremultiply", NULL },
+		      STDIN_FILENO, STDOUT_FILENO);
+
+	bff_wait(pid, "blind-unpremultiply");
+
+	bff_end();
+
+	return 0;
+}
